d5624982e5fff97f44fb43543765b3692521ca02
Florian Schubert <florian.schubert.sg@gmail.com>
2023-09-25 00:38:23 +0200

    Removed Kanata

--- home.nix
+++ home.nix
@@ -36,31 +36,8 @@
     dunst
     waybar
     btop
-    kanata
   ];
 
-  # programs.thunderbird.enable = true;
-
-  programs.swaylock = {
-    enable = true;
-    # settings = {
-    #   daemonize = true;
-    #   clock = true;
-    #   ignore-empty-password = true;
-    # };
-  };
-
-  # services.swayidle = {
-  #   systemdTarget = "hyprland-session.target";
-  #   enable = true;
-  #   events = [
-  #     { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock";}
-  #     { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock";}
-  #   ];
-  #   timeouts = [
-  #     { timeout = 20; command = "${pkgs.swaylock}/bin/swaylock -fF";}
-  #   ];
-  # };
 
   programs.fish.enable=true;
   # programs.fish.loginShellInit="${pkgs.hyprland}/bin/Hyprland";
@@ -68,14 +45,5 @@
     ls = "lsd";
     gs = "git status";
     gl = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-  };
-
-  systemd.user.services.kanata = {
-    Unit.Description = "Kanata Keyboard Manager";
-    Install.WantedBy=["default.target"];
-    Service = {
-      Restart="always";
-      ExecStart = "${pkgs.kanata}/bin/kanata --cfg /home/florian/.config/kanata.kbd";
-    };
   };
 }
