97da8dda7cf6a4b32c5a380a8a8d24c527f74bf5
Florian Schubert <florian.schubert.sg@gmail.com>
2023-10-09 12:11:13 +0200

    Removed Material Theme from Codium

--- editor/codium.nix
+++ editor/codium.nix
@@ -3,7 +3,6 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
-      zhuangtongfa.material-theme
       rust-lang.rust-analyzer
       jnoortheen.nix-ide
     ];
